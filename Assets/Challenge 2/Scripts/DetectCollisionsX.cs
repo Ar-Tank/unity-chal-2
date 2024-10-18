d529655abf690c6cf26cf814ca7fee57344bb19d
Ar-Tank <b00162883@mytudublin.ie>
2024-10-18 13:29:33 +0100

    Add files via upload

---
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // Only destroy the ball when it collides with the dog
+        if (other.CompareTag("Dog"))  // checks if the collided object is tagged as "Dog"
+        {
+            Destroy(gameObject);
+        }
+    }
+}
